063f6ffbf5d66ae2fa7ca16ae3650ab88a946b5d
Dennis Renz <dennis@r-renz.de>
2022-05-13 21:39:49 +0200

    Add example where retrieved object is inconsistent with filter

--- EfCoreNestedDbContextScopes/Program.cs
+++ EfCoreNestedDbContextScopes/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.EntityFrameworkCore;
+
+using var outerContext = new MovieDbContext();
+
+outerContext.Database.EnsureDeleted();
+outerContext.Database.EnsureCreated();
+
+outerContext.Movies.Add(new("Action Movie"));
+outerContext.SaveChanges();
+
+using (var innerContext = new MovieDbContext())
+{
+    innerContext.Movies.Single().Title = "Documentary";
+    innerContext.SaveChanges();
+}
+
+var documentary = outerContext.Movies.Single(movie => movie.Title == "Documentary");
+Console.WriteLine($"{nameof(documentary)} title is {documentary.Title}");
+
+class MovieDbContext : DbContext
+{
+    public DbSet<Movie> Movies => Set<Movie>();
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite("DataSource=movies.db");
+    }
+}
+
+class Movie
+{
+    public int Id { get; set; }
+
+    public string Title { get; set; }
+
+    public Movie(string title) => Title = title;
+}
